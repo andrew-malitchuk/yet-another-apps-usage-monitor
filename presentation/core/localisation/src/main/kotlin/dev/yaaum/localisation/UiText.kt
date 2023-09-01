7f49e279a97065dbc20309f066fd0c8fff021a3b
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-09-01 18:04:11 +0300

    YAAUM-3 Add UiText

--- presentation/core/localisation/src/main/kotlin/dev/yaaum/localisation/UiText.kt
+++ presentation/core/localisation/src/main/kotlin/dev/yaaum/localisation/UiText.kt
@@ -5,15 +5,45 @@ import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 
-// TODO: cover kdoc
+/**
+ * Wrapper over String; Main goal is to use string resources in domain and data (hypothetically)
+ * layers.
+ *
+ * Simple use case: we have general, localised error string in `strings.xml` and some use case
+ * wants to pass human-readable error vai string.xml entry.
+ */
 @Suppress("SpreadOperator")
 sealed class UiText {
+
+    /**
+     * Wrapper over vanilla String.
+     *
+     * Use case: String from API
+     *
+     * @param value
+     */
     data class DynamicString(val value: String) : UiText()
+
+    /**
+     * Container for string resource. Contains _only_ string id and optional arg.
+     *
+     * Use case: need to pass some general message from domain to ui using string res
+     *
+     * @param resId string resource id
+     * @param args optional arguments for string formatter
+     */
     class StringResource(
         @StringRes val resId: Int,
         vararg val args: Any,
     ) : UiText()
 
+    /**
+     * Simple magic. If we call this fun in Composable, [StringResource] will be convert to formatter
+     * string using `stringResource`; otherwise, if we have [DynamicString] - it will do nothing
+     * cos we already have string
+     *
+     * @return String
+     */
     @Composable
     fun asString(): String {
         return when (this) {
@@ -22,6 +52,13 @@ sealed class UiText {
         }
     }
 
+    /**
+     * Simple magic. If we call this fun in non composable, [StringResource] will be convert to formatter
+     * string using `context.getString`; otherwise, if we have [DynamicString] - it will do nothing
+     * cos we already have string
+     *
+     * @return String
+     */
     fun asString(context: Context): String {
         return when (this) {
             is DynamicString -> value
