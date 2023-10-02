7af786a37277fda0e7752514c8beffda2e1d5664
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-10-02 20:59:45 +0300

    YAAUM-40 Fix issues

--- presentation/feature/main/src/main/kotlin/dev/yaaum/presentation/feature/main/navigation/route/MainRoute.kt
+++ presentation/feature/main/src/main/kotlin/dev/yaaum/presentation/feature/main/navigation/route/MainRoute.kt
@@ -2,8 +2,6 @@ package dev.yaaum.presentation.feature.main.navigation.route
 
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
-import cafe.adriel.voyager.navigator.LocalNavigator
-import cafe.adriel.voyager.navigator.currentOrThrow
 import dev.yaaum.presentation.feature.main.screen.composable.main.MainScreen
 
 /**
@@ -12,7 +10,6 @@ import dev.yaaum.presentation.feature.main.screen.composable.main.MainScreen
 class MainRoute : Screen {
     @Composable
     override fun Content() {
-        val navigator = LocalNavigator.currentOrThrow
-        MainScreen(navigator)
+        MainScreen()
     }
 }
--- presentation/feature/main/src/main/kotlin/dev/yaaum/presentation/feature/main/screen/composable/main/MainScreen.kt
+++ presentation/feature/main/src/main/kotlin/dev/yaaum/presentation/feature/main/screen/composable/main/MainScreen.kt
@@ -1,15 +1,20 @@
 package dev.yaaum.presentation.feature.main.screen.composable.main
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.tab.CurrentTab
 import cafe.adriel.voyager.navigator.tab.TabNavigator
 import com.theapache64.rebugger.Rebugger
 import dev.yaaum.presentation.core.ui.theme.YaaumTheme
+import dev.yaaum.presentation.core.ui.theme.common.YaaumTheme
 import dev.yaaum.presentation.feature.main.navigation.tab.AddTab
 import dev.yaaum.presentation.feature.main.navigation.tab.ApplicationListTab
 import dev.yaaum.presentation.feature.main.navigation.tab.HeathTab
@@ -36,7 +41,19 @@ fun MainScreen() {
                     }
                 },
                 bottomBar = {
-                    BottomNavigation {
+                    BottomNavigation(
+                        backgroundColor = YaaumTheme.colors.surface,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(
+                                RoundedCornerShape(
+                                    YaaumTheme.corners.medium,
+                                    YaaumTheme.corners.medium,
+                                    0.dp,
+                                    0.dp,
+                                ),
+                            ),
+                    ) {
                         TabNavigationItem(HeathTab)
                         TabNavigationItem(ApplicationListTab)
                         TabNavigationItem(AddTab)
