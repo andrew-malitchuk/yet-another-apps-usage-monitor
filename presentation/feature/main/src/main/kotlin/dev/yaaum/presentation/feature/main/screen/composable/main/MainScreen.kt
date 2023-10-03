186e8f0f3e987fa3d508bbfe0594125d4c8bda5a
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-10-03 21:30:17 +0300

    YAAUM-40 Fix issues

--- presentation/feature/main/src/main/kotlin/dev/yaaum/presentation/feature/main/screen/composable/main/MainScreen.kt
+++ presentation/feature/main/src/main/kotlin/dev/yaaum/presentation/feature/main/screen/composable/main/MainScreen.kt
@@ -1,18 +1,21 @@
 package dev.yaaum.presentation.feature.main.screen.composable.main
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.tab.CurrentTab
 import cafe.adriel.voyager.navigator.tab.TabNavigator
 import com.theapache64.rebugger.Rebugger
+import dev.yaaum.presentation.core.ui.etc.NoRippleTheme
 import dev.yaaum.presentation.core.ui.theme.YaaumTheme
 import dev.yaaum.presentation.core.ui.theme.common.YaaumTheme
 import dev.yaaum.presentation.feature.main.navigation.tab.AddTab
@@ -20,6 +23,7 @@ import dev.yaaum.presentation.feature.main.navigation.tab.ApplicationListTab
 import dev.yaaum.presentation.feature.main.navigation.tab.HeathTab
 import dev.yaaum.presentation.feature.main.navigation.tab.SettingsTab
 import dev.yaaum.presentation.feature.main.navigation.tab.StatisticTab
+import dev.yaaum.presentation.feature.main.screen.composable.ui.tab.Foo
 import dev.yaaum.presentation.feature.main.screen.composable.ui.tab.TabNavigationItem
 
 @Composable
@@ -30,38 +34,35 @@ fun MainScreen() {
     YaaumTheme {
         TabNavigator(
             HeathTab,
-        ) { tabNavigator ->
-            Scaffold(
-                content = {
-                    Column(
-                        modifier = Modifier
-                            .padding(it),
-                    ) {
-                        CurrentTab()
-                    }
-                },
-                bottomBar = {
+        ) {
+            Box(
+                modifier =
+                Modifier.fillMaxSize(),
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .align(Alignment.Center),
+                ) {
+                    CurrentTab()
+                }
+                CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
                     BottomNavigation(
                         backgroundColor = YaaumTheme.colors.surface,
                         modifier = Modifier
                             .fillMaxWidth()
-                            .clip(
-                                RoundedCornerShape(
-                                    YaaumTheme.corners.medium,
-                                    YaaumTheme.corners.medium,
-                                    0.dp,
-                                    0.dp,
-                                ),
-                            ),
+                            .padding(YaaumTheme.spacing.small)
+                            .clip(YaaumTheme.shapes.medium)
+                            .align(Alignment.BottomCenter),
                     ) {
                         TabNavigationItem(HeathTab)
                         TabNavigationItem(ApplicationListTab)
-                        TabNavigationItem(AddTab)
+                        Foo(AddTab)
                         TabNavigationItem(StatisticTab)
                         TabNavigationItem(SettingsTab)
                     }
-                },
-            )
+                }
+            }
         }
     }
 }
