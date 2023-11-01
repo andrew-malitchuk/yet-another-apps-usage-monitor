939e97957d602ee79b497e7fe1c3b0387a08deea
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-11-01 21:27:22 +0200

    YAAUM-40 Add ApplicationDetalizationScreen

--- presentation/feature/applications/src/main/kotlin/dev/yaaum/presentation/feature/applications/screen/detalization/content/fetched/ApplicationDetalizationFetchedContent.kt
+++ presentation/feature/applications/src/main/kotlin/dev/yaaum/presentation/feature/applications/screen/detalization/content/fetched/ApplicationDetalizationFetchedContent.kt
@@ -1,11 +1,23 @@
 package dev.yaaum.presentation.feature.applications.screen.detalization.content.fetched
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dev.yaaum.presentation.core.ui.theme.YaaumTheme
+import dev.yaaum.presentation.core.ui.theme.common.YaaumTheme
 
 @Composable
-fun ApplicationDetalizationFetchedContent() = Unit
+fun ApplicationDetalizationFetchedContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(YaaumTheme.colors.background),
+    ) {
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
