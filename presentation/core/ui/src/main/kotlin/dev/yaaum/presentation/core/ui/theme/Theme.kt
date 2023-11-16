ae6c7cd86a0c66ca8c8f64961344dd4c1beeab5e
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-11-16 17:14:59 +0200

    YAAUM-40 Add Dynamic Colors

--- presentation/core/ui/src/main/kotlin/dev/yaaum/presentation/core/ui/theme/Theme.kt
+++ presentation/core/ui/src/main/kotlin/dev/yaaum/presentation/core/ui/theme/Theme.kt
@@ -1,17 +1,40 @@
 package dev.yaaum.presentation.core.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import dev.yaaum.presentation.core.ui.theme.ext.toYaaumColors
 
 @Composable
-@Suppress("FunctionNaming", "unused")
+@Suppress("FunctionNaming", "unused", "OptionalWhenBraces")
 fun YaaumTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
     val systemUiController = rememberSystemUiController()
+
+    //
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colorScheme = when {
+        dynamicColor && useDarkTheme -> {
+            dynamicDarkColorScheme(LocalContext.current)
+        }
+
+        dynamicColor && !useDarkTheme -> {
+            dynamicLightColorScheme(LocalContext.current)
+        }
+
+        else -> {
+            null
+        }
+    }
+    colorScheme?.toYaaumColors() ?: baseDarkColorPalette
+    //
 
     val colors = if (useDarkTheme) {
         baseDarkColorPalette
---
+++ presentation/core/ui/src/main/kotlin/dev/yaaum/presentation/core/ui/theme/ext/ColorScheme.kt
@@ -0,0 +1,16 @@
+package dev.yaaum.presentation.core.ui.theme.ext
+
+import dev.yaaum.presentation.core.ui.theme.common.YaaumColors
+
+fun androidx.compose.material3.ColorScheme.toYaaumColors(): YaaumColors {
+    return YaaumColors(
+        primary = this.primary,
+        onPrimary = this.onPrimary,
+        secondary = this.secondary,
+        onSecondary = this.onSecondary,
+        background = this.background,
+        onBackground = this.onBackground,
+        surface = this.secondaryContainer,
+        onSurface = this.onSecondaryContainer,
+    )
+}
